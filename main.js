b5d21b003c254f7a4185fcfbabeb7015880996aa
Leonardo Pita <Leonardopita02@gmail.com>
2022-08-25 09:51:04 -0300

    From exercise 1 to 4

---
+++ main.js
@@ -0,0 +1,56 @@
+
+//==============================   JAVASCRIPT 1 ==================================================//
+// --------------------------- JAVASCRIPT 1 EJ 3 -------------------------------------------------//
+
+// Declarar variables para representar la información de un carnet de conducir.
+// Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.
+
+// let persona    = "BART SIMPSON";
+// let domicilio  = "742 EVERGREEN";
+// let pais       = "USA";
+// let nacimiento = "02-11-70";
+// const codigo   = "B47U89RE243";
+// let carnet = "Codigo: "+codigo+" "+
+//              "Nombre: "+persona+" "+
+//              "Pais: "+pais+" "+
+//              "Nacimiento: "+nacimiento+" "+
+//              "Domicilio: "+domicilio;
+// console.log(carnet);
+
+// --------------------------- JAVASCRIPT 1 EJ 4 -----------------------------------------------//
+
+// Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. Luego, concatenar 
+// dichas entradas y efectuar una salida por alerta.
+
+// let integrante1 = prompt('INGRESAR 1er INTREGANTE');
+// let integrante2 = prompt('INGRESAR 2do INTREGANTE');
+// let integrante3 = prompt('INGRESAR 3er INTREGANTE');
+// let integrante4 = prompt('INGRESAR 4to INTREGANTE');
+// let integrante5 = prompt('INGRESAR 5to INTREGANTE');
+// let familia =   "1) "+integrante1+" "+
+//                 "2) "+integrante2+" "+
+//                 "3) "+integrante3+" "+
+//                 "4) "+integrante4+" "+
+//                 "5) "+integrante5;
+// console.log(familia);
+
+// --------------------------- JAVASCRIPT 1 EJ 5 -------------------------------------------------//
+
+// Solicitar al usuario uno o más precios.
+// Luego, efectuar un descuento en porcentajes (20%, 30%, etcétera) sobre los valores ingresados, y realizar una salida.
+
+// let primerPrecio = prompt("Ingrese el pimer precio");
+// let segundoPrecio = prompt("Ingrese el segundo precio");
+
+// if (primerPrecio >= 0) {
+//     console.log("Tu primer precio es "+primerPrecio+" y el descuento es "+primerPrecio * 0.2+", que es un 20%."+
+//     "\nTu segundo precio es "+segundoPrecio+" y el descuento es "+segundoPrecio * 0.3+", que es un 30%."); 
+// }
+
+
+//==============================   JAVASCRIPT 2 ==================================================//
+// --------------------------- JAVASCRIPT 2 EJ 1 -------------------------------------------------//
+
+// Solicitar al usuario un (1) nombre.
+// Si el mismo es igual a otro nombre almacenado en una variable, realizar una salida por alerta con el mensaje “Fui yo”. 
+// Caso contrario, la salida será “Yo no fui”.
